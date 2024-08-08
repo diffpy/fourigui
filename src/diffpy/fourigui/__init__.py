b67b7c0c5ab8388175ca4286bea35de6f7bb4ad2
Sam Andrello <samqandrello@gmail.com>
2024-08-08 14:06:05 -0400

    remove headers for later addition

--- src/diffpy/fourigui/__init__.py
+++ src/diffpy/fourigui/__init__.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
-##############################################################################
-#
-# (c) 2024 The Trustees of Columbia University in the City of New York.
-# All rights reserved.
-#
-# File coded by: Billinge Group members and community contributors.
-#
-# See GitHub contributions for a more detailed list of contributors.
-# https://github.com/diffpy/fourigui/graphs/contributors
-#
-# See LICENSE.rst for license information.
-#
-##############################################################################
-
 """Tool for visualizing 3D diffraction and PDF Images"""
 
 # from . import fourigui
